b6cdb0f1640c67b98311af6e11d7b6c2fc938199
markas11 <37185104+markas11@users.noreply.github.com>
2025-12-24 10:41:25 +0100

    Add files via upload

---
+++ world-tourist-passport-mvp-v2/README.md
@@ -0,0 +1,2 @@
+# World Tourist Passport MVP
+Azure Functions (.NET 8) backend MVP.
---
+++ world-tourist-passport-mvp-v2/src/PassportFunctions/ApiFunctions.cs
@@ -0,0 +1,42 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using System.Net;
+
+public class ApiFunctions
+{
+    [Function("AdminSeedPlaces")]
+    public HttpResponseData AdminSeedPlaces(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "admin/places/seed")] HttpRequestData req)
+    {
+        var res = req.CreateResponse(HttpStatusCode.OK);
+        res.WriteString("{\"status\":\"ok\",\"message\":\"places seeded\"}");
+        return res;
+    }
+
+    [Function("AdminIssuePassport")]
+    public HttpResponseData AdminIssuePassport(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "admin/passports/issue")] HttpRequestData req)
+    {
+        var res = req.CreateResponse(HttpStatusCode.OK);
+        res.WriteString("{\"passportId\":\"demo\",\"activationToken\":\"demo-token\"}");
+        return res;
+    }
+
+    [Function("ClaimPassport")]
+    public HttpResponseData ClaimPassport(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "passports/claim")] HttpRequestData req)
+    {
+        var res = req.CreateResponse(HttpStatusCode.OK);
+        res.WriteString("{\"status\":\"claimed\"}");
+        return res;
+    }
+
+    [Function("ScanPlace")]
+    public HttpResponseData ScanPlace(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = "places/scan")] HttpRequestData req)
+    {
+        var res = req.CreateResponse(HttpStatusCode.OK);
+        res.WriteString("{\"status\":\"scanned\"}");
+        return res;
+    }
+}
